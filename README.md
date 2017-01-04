39c4d1a6c16803a19d047f98c3ea1e1a9eef6d29
emag <lanabe.lanabe@gmail.com>
2017-01-04 22:00:20 +0900

    It works!

--- README.md
+++ README.md
@@ -21,3 +21,10 @@ java -jar addition/target/addition-swarm.jar -Dswarm.http.port=8180
 ``` sh
 java -jar calc/target/calc-swarm.jar 
 ```
+
+### Access the Calc API
+
+``` sh
+$ curl 'localhost:8080?i1=10&i2=20'
+{"result" : 30}⏎
+```
--- calc/src/main/java/calc/AdditionService.java
+++ calc/src/main/java/calc/AdditionService.java
@@ -1,7 +1,7 @@
 package calc;
 
-import org.wildfly.swarm.cdi.jaxrsapi.Service;
-import org.wildfly.swarm.cdi.jaxrsapi.ServiceClient;
+import org.wildfly.swarm.client.jaxrs.Service;
+import org.wildfly.swarm.client.jaxrs.ServiceClient;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -15,7 +15,7 @@ public interface AdditionService extends ServiceClient<AdditionService> {
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  String add(@QueryParam("i1") int i1,
-             @QueryParam("i2") int i2);
+  String add(@QueryParam("i1") Integer i1,
+             @QueryParam("i2") Integer i2);
 
 }
